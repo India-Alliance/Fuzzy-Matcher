09a07b824a1486c32edfb6770c44fcf0a69588a3
Siddharth Dangwal <maximumendurance1965@gmail.com>
2020-07-30 16:20:39 +0530

    Functions for bulk handling of data

--- app.py
+++ app.py
@@ -7,13 +7,25 @@ app=Flask(__name__)
 def upload():
 	return render_template("home.html")
 
+@app.route("/uploadSuccess", methods=["POST"])
+def	uploadSuccess():
+	print("reached")
+	f=request.files['file']
+	f.save(f.filename)
+	standardised_name = standardise.standardiseList(f.filename)
+	print(standardised_name)
+	return render_template("success.html", tables=[standardised_name.to_html(classes='data', header="true")]) 	
+
 @app.route("/success", methods=["POST"])
 def show_disambiguated_name():
 	entered_name = request.form['name-entry-field']
-	print(entered_name)
-	standardised_name = standardise.standardise(entered_name)
-	print(standardised_name)
+	standardised_name = standardise.standardiseName(entered_name)
 	return render_template("success.html", tables=[standardised_name.to_html(classes='data', header="true")]) 	
+
+@app.route("/download")
+def download():
+	filename = f.filename.split('.')[0] + "-standardised.pdf"
+	return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- standardise.py
+++ standardise.py
@@ -5,11 +5,20 @@ import time
 from initialise_standard_univ_list import initialise_standard_univ_list
 from convert_to_data_frame import convert_to_data_frame
 
-def standardise(uploaded_name):
-	t1 = time.time()
+def get_standard_univs_names():
 	standard_univs = initialise_standard_univ_list()
-	standard_univs_names = standard_univs['Name']
-	# uploaded_names = ['Cambridge', 'IITB', 'L V Prasad']
-	matched_names= process.extractOne(uploaded_name, standard_univs_names)
-	return convert_to_data_frame([matched_names], [uploaded_name])
-	# print("Total Time taken :", time.time()-t1)
+	return standard_univs['Name']
+
+def standardiseName(uploaded_name):
+	standard_univs_names = get_standard_univs_names()
+	matched_names=process.extract(uploaded_name, standard_univs_names)
+	return convert_to_data_frame(matched_names, uploaded_name)
+
+def standardiseList(file):
+	uploaded_data = pd.read_csv(file)
+	uploaded_names = uploaded_data['name']
+	print(uploaded_names)
+	standard_univs_names = get_standard_univs_names()
+	matched_names = [process.extractOne(uploaded_name, standard_univs_names)
+					for uploaded_name in uploaded_names]
+	return convert_to_data_frame(matched_names, uploaded_names)
