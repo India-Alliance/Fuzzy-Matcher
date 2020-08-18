03110adc71f25c0afa9f1ed97dac6a6ba013b0f7
Siddharth Dangwal <maximumendurance1965@gmail.com>
2020-08-18 13:37:06 +0530

    Reformats code as per flake8 and adds tqdm

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
-from flask import *
+from flask import render_template, request, Flask
 import standardise
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def upload():
-	return render_template("home.html")
+    return render_template("home.html")
+
 
 @app.route("/uploadSuccess", methods=["POST"])
-def	uploadSuccess():
-	print("reached")
-	f=request.files['file']
-	f.save(f.filename)
-	standardised_name = standardise.standardiseList(f.filename)
-	print(standardised_name)
-	return render_template("success.html", tables=[standardised_name.to_html(classes='data', header="true")]) 	
+def uploadSuccess():
+    f = request.files['file']
+    f.save(f.filename)
+    standardised_name = standardise.standardiseList(f.filename)
+    print(standardised_name)
+    return render_template(
+        "success.html", tables=[
+            standardised_name.to_html(
+                classes='data', header="true")])
+
 
 @app.route("/success", methods=["POST"])
 def show_disambiguated_name():
-	entered_name = request.form['name-entry-field']
-	standardised_name = standardise.standardiseName(entered_name)
-	return render_template("success.html", tables=[standardised_name.to_html(classes='data', header="true")]) 	
+    entered_name = request.form['name-entry-field']
+    standardised_name = standardise.standardiseName(entered_name)
+    return render_template(
+        "success.html", tables=[
+            standardised_name.to_html(
+                classes='data', header="true")])
 
-@app.route("/download")
-def download():
-	filename = f.filename.split('.')[0] + "-standardised.pdf"
-	return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
-	app.run(debug=True)
-
+    app.run(debug=True)
--- initialise_standard_univ_list.py
+++ initialise_standard_univ_list.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
+
 def initialise_standard_univ_list():
-	all_univs = pd.read_csv('https://raw.githubusercontent.com/MaximumEndurance/Fuzzy-Matcher/master/GridUnivsData/grid.csv')
+    path = 'https://raw.githubusercontent.com/MaximumEndurance/'
+    sub_path = 'Fuzzy-Matcher/master/GridUnivsData/'
+    # path and sub_path declared to match max line length rule which
+    # was breaking because of the length of the URL string
+
+    all_univs = pd.read_csv(f"{path}{sub_path}grid.csv")
 
     # Using acronyms to make the fuzzy matcher acronym sensitive as well
-	acronyms = pd.read_csv('https://raw.githubusercontent.com/MaximumEndurance/Fuzzy-Matcher/master/GridUnivsData/acronyms.csv')
-	acronyms.head()
-	acronyms = acronyms.rename(columns={"grid_id": "ID"})
+    acronyms = pd.read_csv(f"{path}{sub_path}acronyms.csv")
+    acronyms.head()
+    acronyms = acronyms.rename(columns={"grid_id": "ID"})
 
-	return pd.merge(all_univs, acronyms, on='ID')
-
+    return pd.merge(all_univs, acronyms, on='ID')
