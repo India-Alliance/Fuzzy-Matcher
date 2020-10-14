4d64fccc2f7ae858b3f56759c4120ce3cf69af1f
aCampello <a.campello@wellcome.ac.uk>
2020-10-14 13:58:03 +0100

    Resolve merge conflict

--- matcher/standardise.py
+++ matcher/standardise.py
@@ -1,4 +1,4 @@
-from fuzzywuzzy import process
+from fuzzywuzzy import process, fuzz
 from fuzzywuzzy.utils import full_process
 import pandas as pd
 import time
@@ -21,27 +21,56 @@ def standardise_name(uploaded_name, standard_univs):
         val = acronym_match.values[0]
         return val, 100, acronym_match.index[0]
 
-    # Alias matches
+    processed_name = full_process(uploaded_name, force_ascii=True)
 
-    alias_match = process.extractOne(uploaded_name, standard_univs['alias'])
-    full_name_match = process.extractOne(uploaded_name, standard_univs['Name'])
+
+    # Name match scores
+    standard_univs['full_name_similarity'] = standard_univs['Name'].apply(
+        lambda x: fuzz.ratio(processed_name, x)
+    )
+    full_name_match_idx = standard_univs['full_name_similarity'].nlargest(n=1, keep='all').index
+
+    full_name_matches = [
+        (row['Name'], row['full_name_similarity'], idx)
+        for idx, row in standard_univs.iloc[full_name_match_idx].iterrows()
+    ]
+
+    # Alias match scores
+    standard_univs['alias_similarity'] = standard_univs['alias'].apply(
+        lambda x: fuzz.ratio(processed_name, x)
+    )
+    alias_match_idx = standard_univs['alias_similarity'].nlargest(n=1, keep='all').index
+
+    # Looks weird to use row['name'] for alias
+    # but we want to return the name in the end
+    # We are comparing the correct similarity though.
+    alias_matches = [
+        (row['Name'], row['alias_similarity'], idx)
+        for idx, row in standard_univs.iloc[alias_match_idx].iterrows()
+    ]
 
     # The tuples - alias_match and full_name_match have three values -
     # (Name Matched, Confidence and Index Number in the standard list)
     # We are here comparing confidence between the full name match and the alias match
     # and returing the one with greater confidence.
 
-    if not alias_match and not full_name_match:
+    if len(alias_matches) == 0 or len(full_name_matches) == 0:
         print("No match for input name in our database")
         return ()
 
     # Since, the aim is to return standard names,
     # in case of an alias value name with higher confidence, we return the corresponding full name
 
-    if (len(alias_match) == 0 or alias_match[1] <= full_name_match[1]):
-        return full_name_match
+    # TODO: This is only returning the FIRST match. What if there are two
+    #  names with the same score?
+
+    print(alias_matches)
+    print(full_name_matches)
+
+    if len(alias_matches) == 0 or alias_matches[0][1] <= full_name_matches[0][1]:
+        return full_name_matches[0]
     else:
-        return (standard_univs.at[alias_match[2], 'Name'], alias_match[1], alias_match[2])
+        return alias_matches[0]
 
 
 def standardise_list(file, column_name_to_standardise='uploaded_names'):
@@ -57,9 +86,10 @@ def standardise_list(file, column_name_to_standardise='uploaded_names'):
     # Initialises list of universities
     standard_univs = initialise_standard_univ_list()
     # Processes list of universities
-    standard_univs['Name'] = standard_univs['Name'].apply(
-        lambda x: full_process(x, force_ascii=True)
-    )
+    for column in ['Name', 'alias']:
+        standard_univs[column] = standard_univs[column].apply(
+            lambda x: full_process(x, force_ascii=True)
+        )
 
     matched_names = [standardise_name(uploaded_name, standard_univs)
                      for uploaded_name in tqdm(uploaded_names)]
