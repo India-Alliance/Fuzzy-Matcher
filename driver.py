74f161ddaf4158440b84cbdaa8fd1097d1e0b3df
Siddharth Dangwal <maximumendurance1965@gmail.com>
2020-11-19 17:01:11 +0530

    Separates validator and matcher

--- driver.py
+++ driver.py
@@ -1,45 +1,62 @@
-import sys
+import sys, os
 from validator.validate_data import validate_csv_dataset
 from matcher.standardise import standardise_list
 import pandas as pd
+import argparse
+import jsonschema
+import json
 
 
-def setup_input_output_paths(path_to_file):
-    path_to_input_file = sys.argv[1] if len(sys.argv) > 1 else 'test.csv'
-    path_to_output_file = sys.argv[2] if len(sys.argv) > 2 else 'log.txt'
-
-    if path_to_input_file is None:
-        if path_to_file is not None:
-            path_to_input_file = path_to_file
-        else:
-            path_to_input_file = 'test.csv'
-
-    if path_to_output_file is None:
-        path_to_output_file = 'log.txt'
-
-    return path_to_input_file, path_to_output_file
+def validate_data(path_to_file):
+    unmatched_entities = validate_csv_dataset(path_to_file,
+                                              log_file='log.txt')
+    print('Detailed logs can be viewed in log.txt')
+    return unmatched_entities
 
 
-def validate_and_suggest_corrections(path_to_file=None,
-                                     column_to_update="affiliationCurrent"):
-    path_to_input_file, path_to_output_file = setup_input_output_paths(path_to_file)
+def load_json_file():
+    HERE = os.path.dirname(os.path.abspath(__file__))
 
-    unmatched_entities = validate_csv_dataset(path_to_input_file,
-                                              log_file=path_to_output_file)
-    print('Entries which were not matched \n\n', unmatched_entities)
+    SCHEMA_FILE = os.path.abspath(
+        os.path.join(HERE, '../Fuzzy-Matcher/data/json-schema/criteria-json-schema.json')
+    )
+    SCHEMA_DIR = os.path.dirname(SCHEMA_FILE)
+
+    with open(SCHEMA_FILE, 'r') as f:
+        SCHEMA = json.load(f)
+    return SCHEMA
+
+
+def match_and_suggest_corrections(unmatched_entities):
     # Now let us try fuzzy matching names to identify
     # typos or non-standard entries.
 
-    # First let's run the university standardiser
+    SCHEMA = load_json_file()
+    columns_to_fuzzy_match = SCHEMA['fuzzy-match']
 
-    uploaded_names_df = pd.DataFrame(
-        {'uploaded_names': unmatched_entities[column_to_update]}
-    )
-    uploaded_names_df.to_csv('entries_to_fuzzy_match.csv')
-
-    standardise_list('entries_to_fuzzy_match.csv',
-                     column_name_to_standardise='uploaded_names')
+    unmatched_entities = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in unmatched_entities.items()]))
+    unmatched_entities.to_csv('entries_to_fuzzy_match.csv')
+    for column_to_fuzzy_match in columns_to_fuzzy_match:
+        standardise_list('entries_to_fuzzy_match.csv',
+                         column_name_to_standardise=column_to_fuzzy_match,
+                         column_entity_type=columns_to_fuzzy_match[column_to_fuzzy_match])
 
 
 if __name__ == "__main__":
-    validate_and_suggest_corrections()
+
+    parser = argparse.ArgumentParser(description='Validate and Fuzzy Match non-standard entries')
+    parser.add_argument("--path_to_standard_file")
+    parser.add_argument("--validate", '-v', action='store_true', help="Validate the data")
+    parser.add_argument("--match", '-m', action='store_true', help="Fuzzy Match the data against the standard list provided")
+    parser.add_argument('path_to_file_to_standardise')
+
+    args = parser.parse_args()
+
+    if args.validate:
+        unmatched_entities = validate_data(args.path_to_file_to_standardise)
+        print(unmatched_entities)
+
+    if args.match:
+        unmatched_entities = validate_data(args.path_to_file_to_standardise)
+        match_and_suggest_corrections(unmatched_entities)
+
--- matcher/standardise.py
+++ matcher/standardise.py
@@ -1,20 +1,21 @@
 from fuzzywuzzy import process, fuzz
 from fuzzywuzzy.utils import full_process
-import pandas as pd
-import time
 from matcher.initialise_standard_univ_list import initialise_standard_univ_list
 from matcher.convert_to_data_frame import convert_to_data_frame
 from tqdm import tqdm
 
+import pandas as pd
+import time
+from argparse import ArgumentParser
 
-def get_standard_univs_names():
-    standard_univs = initialise_standard_univ_list()
-    return standard_univs['Name']
+def get_standard_names_names():
+    standard_names = initialise_standard_univ_list()
+    return standard_names['Name']
 
 
-def standardise_name(uploaded_name, standard_univs):
-    acronym_match = standard_univs.loc[
-        standard_univs['acronym'] == uploaded_name, 'Name'
+def standardise_name(uploaded_name, standard_names):
+    acronym_match = standard_names.loc[
+        standard_names['acronym'] == uploaded_name, 'Name'
     ]
 
     if not acronym_match.empty:
@@ -24,28 +25,28 @@ def standardise_name(uploaded_name, standard_univs):
     processed_name = full_process(uploaded_name, force_ascii=True)
 
     # Name match scores
-    standard_univs['full_name_similarity'] = standard_univs['Name'].apply(
+    standard_names['full_name_similarity'] = standard_names['Name'].apply(
         lambda x: (fuzz.ratio(processed_name, x) if pd.notna(x) else 0)
     )
-    full_name_match_idx = standard_univs['full_name_similarity'].nlargest(n=1, keep='all').index
+    full_name_match_idx = standard_names['full_name_similarity'].nlargest(n=1, keep='all').index
 
     full_name_matches = [
         (row['Name'], row['full_name_similarity'], idx)
-        for idx, row in standard_univs.iloc[full_name_match_idx].iterrows()
+        for idx, row in standard_names.iloc[full_name_match_idx].iterrows()
     ]
 
     # Alias match scores
-    standard_univs['alias_similarity'] = standard_univs['alias'].apply(
+    standard_names['alias_similarity'] = standard_names['alias'].apply(
         lambda x: (fuzz.ratio(processed_name, x) if pd.notna(x) else 0)
     )
-    alias_match_idx = standard_univs['alias_similarity'].nlargest(n=1, keep='all').index
+    alias_match_idx = standard_names['alias_similarity'].nlargest(n=1, keep='all').index
 
     # Looks weird to use row['name'] for alias
     # but we want to return the name in the end
     # We are comparing the correct similarity though.
     alias_matches = [
         (row['Name'], row['alias_similarity'], idx)
-        for idx, row in standard_univs.iloc[alias_match_idx].iterrows()
+        for idx, row in standard_names.iloc[alias_match_idx].iterrows()
     ]
 
     # The tuples - alias_match and full_name_match have three values -
@@ -69,7 +70,8 @@ def standardise_name(uploaded_name, standard_univs):
         return alias_matches[0]
 
 
-def standardise_list(file, column_name_to_standardise='uploaded_names'):
+
+def standardise_list(file, column_name_to_standardise, column_entity_type):
     t1 = time.time()
     uploaded_data = pd.read_csv(file)
 
@@ -77,22 +79,55 @@ def standardise_list(file, column_name_to_standardise='uploaded_names'):
         print(f"No column {column_name_to_standardise} in Uploaded Data")
         return pd.DataFrame()
 
+    print(f'Generating suggestions for {column_name_to_standardise}')
     uploaded_names = uploaded_data[column_name_to_standardise].dropna().unique()
 
-    # Initialises list of universities
-    standard_univs = initialise_standard_univ_list()
+    standard_names = None
+    if column_entity_type == 'affiliation':
+        # Initialises list of universities
+        standard_names = initialise_standard_univ_list()
+    else:
+        print('Column Type not supported')
+        return pd.DataFrame()
+
     # Processes list of universities
     for column in ['Name', 'alias']:
-        standard_univs[column] = standard_univs[column].apply(
+        standard_names[column] = standard_names[column].apply(
             lambda x: full_process(x, force_ascii=True)
         )
 
-    matched_names = [standardise_name(uploaded_name, standard_univs)
+    matched_names = [standardise_name(uploaded_name, standard_names)
                      for uploaded_name in tqdm(uploaded_names)]
 
     matched_names_df = convert_to_data_frame(matched_names, uploaded_names)
     matched_names_df['Correct (1/0)'] = pd.Series()
     # The above column has to be manually filled in by the reviewer
-    matched_names_df.to_csv('Suggestions.csv')
+    matched_names_df.to_csv(f'{column_name_to_standardise}Suggestions.csv')
+    print(f'Suggestions for {column_name_to_standardise} written into {column_name_to_standardise}Suggestions.csv')
     print(f"Total time taken for Fuzzy Matching - {time.time() - t1} seconds")
     return matched_names_df
+
+
+# This is for independent usage of the standardiser and for a custom standardising process
+def generic_standardiser(uploaded_list, standard_list):
+        uploaded_list = pd.read_csv(uploaded_list)
+        standard_list = pd.read_csv(standard_list)
+        processed_names = [full_process(name, force_ascii=True) for name in uploaded_list]
+        suggestions = [standard_list.apply(
+        lambda x: (fuzz.ratio(processed_name, x) if pd.notna(x) else 0))
+        for processed_name in processed_names]
+
+        print(suggestions)
+
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('path_to_file_to_standardise')
+    parser.add_argument("path_to_standard_file")
+    args = parser.parse_args()
+
+    if(len(args) != 2):
+        print('Enter path of file to standardise and path to file containing the standard names')    
+    generic_standardiser(args.path_to_file_to_standardise, args.path_to_standard_file)
+    
